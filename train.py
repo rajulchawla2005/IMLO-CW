def8cfbd16bd6c92d934cffc279dc31be52abf6e
rajulchawla2005 <rajulchawla2005@gmail.com>
2025-05-17 16:41:42 +0100

    first nn model
    flatten, then linear a few times
    
    only getting like 40% for accuracy after 5 epochs, not great

--- train.py
+++ train.py
@@ -12,18 +12,95 @@ training_data = datasets.CIFAR10(
     transform=ToTensor()
 )
 
+train_set, val_set = torch.utils.data.random_split(training_data, [40000, 10000])
+
+epochs = 5
 device = torch.device("cpu")
 batch_size = 64
-dataloader = DataLoader(training_data, batch_size=batch_size)
+train_dataloader = DataLoader(train_set, batch_size=batch_size)
+test_dataloader = DataLoader(val_set, batch_size=batch_size)
 
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.stack = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(
+                in_features=3*32*32,
+                out_features=1024
+            ),
+            nn.ReLU(),
+            nn.Linear(
+                in_features=1024,
+                out_features=256
+            ),
+            nn.ReLU(),
+            nn.Linear(
+                in_features=256,
+                out_features=10
+            )
+        )
+
+    def forward(self, x):
+        return self.stack(x)
+
+def train():
+    size = len(train_dataloader.dataset)
+    # training mode
+    model.train()
+    # for every item in the training set
+    for batch, (X, y) in enumerate(train_dataloader):
+        # put the input and label on the cpu
+        X, y = X.to(device), y.to(device)
+        # make a guess
+        guess = model(X)
+        # calculate a loss
+        loss = loss_fn(guess, y)
+
+        # backpropagation
+        loss.backward()
+        optimiser.step()
+        optimiser.zero_grad()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), (batch + 1) * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+def train_test():
+    size = len(test_dataloader.dataset)
+    num_batches = len(test_dataloader)
+    # testing mode
+    model.eval()
+    test_loss, correct = 0, 0
+    # for every item in the test set
+    with torch.no_grad():
+        for X, y in test_dataloader:
+            # put input and label on cpu
+            X, y = X.to(device), y.to(device)
+            # make a guess
+            guess = model(X)
+            # track loss and correctness
+            test_loss += loss_fn(guess, y).item()
+            correct += (guess.argmax(1) == y).type(torch.float).sum().item()
+    # average 
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
 if __name__ == "__main__":
     # setup model
     # train model
     # store into .pth file
-    for X, y in dataloader:
-        print(f"Shape of X [N, C, H, W]: {X.shape}")
-        print(f"Shape of y: {y.shape} {y.dtype}")
-        break
 
-    
+    model = NeuralNetwork().to(device)
+    loss_fn = nn.CrossEntropyLoss()
+    optimiser = torch.optim.SGD(model.parameters(), lr=0.01)
+
+    for t in range(epochs):
+        print(f"Epoch {t+1}\n")
+        train()
+        train_test()
+    print("Done!")
+
+    torch.save(model.state_dict(), "model.pth")
+    print("Saved PyTorch Model State to model.pth")
